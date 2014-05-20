e94afb4f14597801cc17d13613ca5a948a6caf05
Administrator <Administrator@172.26.114.218>
2014-05-20 23:10:55 +0800

    player类

---
+++ src/UI/Player.java
@@ -0,0 +1,17 @@
+package UI;
+
+public class Player {
+	//总局数，平均得分（玩过的所有局），最高连击数量，最高得分，单局最长无敌时间
+
+    String name;
+    String password;
+    int gamecount;
+    int avgscore;
+    int hitcount,maxhitcount;
+    int maxscore,score;
+    int maxgodtime,godtime;
+    
+    void 
+
+	}
+}
